55b63585fb7ef9cf4ece70fa68578593a340816f
Hussein <aisackhussein@gmail.com>
2024-08-01 14:27:25 +0300

    first commit

---
+++ KilimaniHackathon/KilimaniHackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  KilimaniHackathonApp.swift
+//  KilimaniHackathon
+//
+//  Created by Muktar Aisak on 01/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct KilimaniHackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
